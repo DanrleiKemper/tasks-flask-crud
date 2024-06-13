9809635ce556de9aa64418cc244d408ec594103b
Danrlei Kemper <danrleikemper95@gmail.com>
2024-06-12 21:24:46 -0300

    created routes to list all tasks and list specific task

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ tasks = []
 #Variavel para iniciar a tarefa com id 1
 task_id_control = 1
 
-# riação da rota task com o metodo POST
+# Criação da rota task com o metodo POST
 # Metodo para criar as tarefas (creat_task())
 @app.route('/tasks', methods=['POST'])
 def creat_task():
@@ -20,6 +20,28 @@ def creat_task():
     task_id_control += 1
     tasks.append(new_task)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+# Metodo para listar todas as tarefas (get_tasks())
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)        
+            }
+    return jsonify(output)
+
+# Metodo para listar somente uma tarefa (get_task())
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+#    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message:" "Tarefa não encontrada"}), 404 
+
 
 
 #modo de execução de desenvolvimento local
