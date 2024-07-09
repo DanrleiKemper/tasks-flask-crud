de5774405b8d5435a92ee052e38722aa90f46cbc
Danrlei Kemper <71983024+DanrleiKemper@users.noreply.github.com>
2024-07-09 20:41:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -42,7 +42,43 @@ def get_task(id):
             return jsonify(t.to_dict())
     return jsonify({"message:" "Tarefa não encontrada"}), 404 
 
+# Metodo para atualização de tarefa (update_task())
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+    #função para percorrer a lista de tarefas para verificar se existe ou não
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    #condição caso a tarefa não for encontrada
+    if task == None:
+        return jsonify({"message:" "Tarefa não encontrada"}), 404 
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
 
+# Metodo para atualização de tarefa (delete_task())
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    #função para percorrer a lista de tarefas para verificar se existe ou não
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    #condição caso a tarefa não for encontrada
+    if not task:
+        return jsonify({"message:" "Tarefa não encontrada"}), 404 
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 #modo de execução de desenvolvimento local
 if __name__ == "__main__":
